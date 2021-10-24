427c6d433c8cd306e7f2646f0bdb3376d6f58cee
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-10-24 21:37:13 +0200

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+
 const App = () => {
  return <h1>Hello from react checklist!</h1>;
  }
+
 ReactDOM.render(<App />, document.getElementById('app'));
